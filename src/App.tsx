bacd2565166c8cbd7fe0d82288de6204ced8c423
Saurav Sah <114904905+SauravSahPS@users.noreply.github.com>
2023-04-16 12:30:16 +0530

    Passing data via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["Delhi", "Bengaluru", "Mumbai", "Chennai", "Kolkata"];
+
   return (
     <div>
-      <ListGroup />
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
