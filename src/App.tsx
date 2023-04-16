d3fb2ce73fa696d4b5231500f86a36abd4ede7e2
Saurav Sah <114904905+SauravSahPS@users.noreply.github.com>
2023-04-16 17:34:15 +0530

    Added childern , onclick, props vs state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
+import Button from "./components/Button";
 
 function App() {
   let items = ["Delhi", "Bengaluru", "Mumbai", "Chennai", "Kolkata"];
 
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+      <Button
+        color="danger"
+        buttonClicked={() => console.log("Button clicked ")}
+      >
+        My Button
+      </Button>
+      <Alert>
+        Hello{" "}
+        <span>
+          <b>World</b>
+        </span>
+      </Alert>
     </div>
   );
 }
