94b57a44988bdc82ec784ea0bcda4b70ef5bccf7
Saurav Sah <114904905+SauravSahPS@users.noreply.github.com>
2023-04-17 20:00:53 +0530

    Day 2 complete working code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 import Button from "./components/Button";
+import DismissingAlert from "./components/DismissingAlert";
+import { useState } from "react";
 
 function App() {
   let items = ["Delhi", "Bengaluru", "Mumbai", "Chennai", "Kolkata"];
@@ -9,19 +11,25 @@ function App() {
     console.log(item);
   };
 
+  let setAlertPopup = "hide";
+
+  const [showAlertPopup, setShowAlert] = useState(false);
+
   return (
     <div>
+      <DismissingAlert
+        showAlertPopup={showAlertPopup}
+        closeAlert={() => setShowAlert(false)}
+      ></DismissingAlert>
+      <Button color="danger" buttonClicked={() => setShowAlert(true)}>
+        My Button
+      </Button>
       <ListGroup
         items={items}
         heading="Cities"
         onSelectItem={handleSelectItem}
       />
-      <Button
-        color="danger"
-        buttonClicked={() => console.log("Button clicked ")}
-      >
-        My Button
-      </Button>
+
       <Alert>
         Hello{" "}
         <span>
